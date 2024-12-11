fe4587ccf6133f83c2f610b358fe06370b04cd4d
loommii <v1061827272@hotmail.com>
2024-12-11 19:10:24 +0800

    feat: go-zero init

---
+++ internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"captchaImageServer/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
